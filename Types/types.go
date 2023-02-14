c78c63377498e1a25b6ca6cfe49f6bc042c84c77
HammamiKhairi <hammamikhairi@gmail.com>
2023-02-14 20:49:09 +0100

    migrated global types to Types/

---
+++ Types/types.go
@@ -0,0 +1,32 @@
+package types
+
+var KEYWORDS []string = []string{
+	"break ", "default ", "func ", "interface", "select",
+	"case", "defer", "go", "map", "struct", "func", "main",
+	"chan", "else", "goto", "package", "switch", "init",
+	"const", "fallthrough", "if", "range", "type", "len",
+	"continue", "for", "import", "return", "var", "make",
+	"panic",
+}
+
+var TYPES []string = []string{
+	"bool", "string", "int", "int8", "int16", "int32",
+	"int64", "uint", "uint8", "uint16", "uint32", "uint64",
+	"uintptr", "float32", "float64", "complex64", "complex128",
+	"byte", "rune", "nil",
+}
+
+var LIBRARIES []string = []string{
+	"fmt", "os", "log", "net", "filepath", "ftp",
+}
+
+// For Token position [inMain, col]
+type Vec2i struct {
+	X, Line int
+}
+
+type Token struct {
+	Kind TokenKind
+	Addr Vec2i
+	Len  int
+}
